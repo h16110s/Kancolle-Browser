d280217e99befc19169a923bb95c6ae80edbc5cf
Hirohito Saito <h_16110s@yahoo.co.jp>
2020-03-21 03:57:51 +0900

    Initial Commit

---
+++ kancolle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kancolle
+//
+//  Created by Hirohito on 2020/03/21.
+//  Copyright © 2020 Hirohito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
